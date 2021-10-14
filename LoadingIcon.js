232c79b93ebfa581ffe5deb6dd8867d5d9d215e7
habituallynate <hello@nateweller.com>
2021-10-14 09:42:00 -0600

    add LoadingIcon component

---
+++ LoadingIcon.js
@@ -0,0 +1,36 @@
+import { BiLoaderAlt } from 'react-icons/bi';
+
+function LoadingIcon(props) {
+
+    const { 
+        isFullScreen = false, 
+        isLarge = false,
+        isSmall = false
+    } = props;
+
+    const icon = (
+        <div className="inline-block animate-spin">
+            <BiLoaderAlt className={ `${isSmall ? 'h-4 w-4' : 'h-8 w-8'} text-indigo-100` } />
+        </div>
+    );
+
+    if (isFullScreen) {
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                { icon }
+            </div>
+        );
+    }
+
+    if (isLarge) {
+        return (
+            <div className="text-center py-24">
+                { icon }
+            </div>
+        );
+    }
+
+    return icon;
+}
+
+export default LoadingIcon;
