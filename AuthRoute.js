327e591ca9339ac9f2a019c5115fc21b4963fc87
habituallynate <hello@nateweller.com>
2021-10-14 09:43:12 -0600

    add AuthRoute component

---
+++ AuthRoute.js
@@ -0,0 +1,19 @@
+import { Route, Redirect } from 'react-router-dom';
+import { useAuth } from './../hooks';
+
+function AuthRoute({ component: Component, ...rest }) {
+
+    const { user } = useAuth();
+
+    return (
+        <Route 
+            { ...rest }
+            render={ (props) => user 
+                ? <Component { ...props } />
+                : <Redirect to={ { pathname: '/login', state: { from: props.location } } } />
+            }
+        />
+    );
+}
+
+export default AuthRoute;
